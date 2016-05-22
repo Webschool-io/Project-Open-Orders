97b3860deab6118c389870ad800d445572664a17
Suissa <jnascimento@gmail.com>
2016-05-21 23:32:52 -0300

    Comprador soh com name

--- server/modules/Comprador/molecules/comprador.js
+++ server/modules/Comprador/molecules/comprador.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const Molecule = {
   name: require('./../atoms/name')
 // , dateBirth: require('./../atoms/dateBirth')
-, products: [{type: Schema.Types.ObjectId, ref: 'products'}]
+// , products: [{type: Schema.Types.ObjectId, ref: 'products'}]
 }
 
 module.exports = new Schema(Molecule);
